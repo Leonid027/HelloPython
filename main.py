7f0e919b6bc62337f9d04c25bebcbe71b28c8922
Leonid027 <49811681+Leonid027@users.noreply.github.com>
2019-04-27 17:43:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello Ploss, pozdravlyayu ss 5!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-    
+#def hello();
+#eturn "Hello Ploss, pozdravlyayu ss 5!"
 def func(string):
   maxlen = 0
-  for x in string.split(' '):
+  for x in string.split('Hello Ploss, pozdravlyayu ss 5!'):
     if len(x)> maxlen:
       maxlen = len(x)
       word = x
   print (string +'\n','Самое длинное слово - ', word, ',', maxlen, ' символов')
  
 func(string = input ('Введите строку: '))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+    
+
