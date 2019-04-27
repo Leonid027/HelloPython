b2ec15fef43e12795dfb9f002056b506c287a737
Leonid027 <49811681+Leonid027@users.noreply.github.com>
2019-04-27 17:47:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+
+
+f = open("lesson.txt")
+r = f.read()
+ 
+# данный код определяет сколько раз встречается самое длинное слово в файле.
+s = max(map(lambda x: (len(x), x), r.replace(',','').replace('.','').split(" ")))
+ 
+print(r,'\n','Самое длинное слово:"',s[1],'" встречается:',r.count(s[1]), 'раз(а)')
+
 #def hello();
 #eturn "Hello Ploss, pozdravlyayu ss 5!"
-def func(string):
-  maxlen = 0
-  for x in string.split('Hello Ploss, pozdravlyayu ss 5!'):
-    if len(x)> maxlen:
-      maxlen = len(x)
-      word = x
-  print (string +'\n','Самое длинное слово - ', word, ',', maxlen, ' символов')
+#def func(string):
+ # maxlen = 0
+ # for x in string.split('Hello Ploss, pozdravlyayu ss 5!'):
+ #   if len(x)> maxlen:
+   #   maxlen = len(x)
+   #   word = x
+ # print (string +'\n','Самое длинное слово - ', word, ',', maxlen, ' символов')
  
 # func(string = input ('Введите строку: '))
 if __name__ == '__main__':
