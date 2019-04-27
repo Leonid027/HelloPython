abd69a4d350507e80f12f5fb7a67ed67c324dcd9
Leonid027 <49811681+Leonid027@users.noreply.github.com>
2019-04-27 18:41:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/begin', methods=['POST']) 
-def begin():
+def begin()
  
-def hello():
+def hello()
    return "Helllllo World!"
 print(hello())
 listWords = hello().split()
